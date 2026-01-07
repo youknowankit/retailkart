e7ff5095e2e41c488cc97ccb1a75b25d706169b3
Ankit Yadav <thisisankityadav@gmail.com>
2026-01-07 11:33:11 +0530

    Update server.js to use userRoute file

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import connectDB from "./database/db.js";
+import userRoute from "./routes/userRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//Sample Route: http://localhost:8000/api/v1/user/register
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
   connectDB();
