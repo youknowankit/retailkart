7225be28c2fba0ec85fd7616e578bac8f08f1dae
Ankit Yadav <thisisankityadav@gmail.com>
2026-01-07 11:20:19 +0530

    Update server.js to use the db.js connectDB for connecting to database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import "dotenv/config";
+import connectDB from "./database/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log("'Server is running at PORT:", PORT);
 });
