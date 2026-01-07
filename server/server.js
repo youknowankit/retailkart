b722c217bd71eaa290b47c2b1a5cee970186e9e5
Ankit Yadav <thisisankityadav@gmail.com>
2026-01-07 10:47:08 +0530

    Use express to run server at a port

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
-console.log("Hello! from server");
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("'Server is running at PORT:", PORT);
+});
