934af51bdd8377dba0eba60c6532eab989ba00ad
Ankit Yadav <thisisankityadav@gmail.com>
2026-01-08 14:12:53 +0530

    Add login route to the userRoute

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { register, reVerify, verify } from "../controllers/userController.js";
+import {
+  login,
+  register,
+  reVerify,
+  verify,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/verify", verify);
 router.post("/reverify", reVerify);
+router.post("/login", login);
 
 export default router;
