42b21fba1b2489933d79eb3c016bce2363ab5d3e
Ankit Yadav <thisisankityadav@gmail.com>
2026-01-08 13:53:53 +0530

    Add verify route to userRoute

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register } from "../controllers/userController.js";
+import { register, verify } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/verify", verify);
 
 export default router;
